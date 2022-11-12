d3ec87554cb376c43b662e09f2413ae7ba0ab425
Dv04 <62090715+Dv04@users.noreply.github.com>
2022-11-13 00:53:31 +0530

    Revert "Dev"

--- Test/temprature-listener.js
+++
@@ -1,9 +0,0 @@
-const { spawn } = require('child_process');
-const temperatures = []; // Store readings
-
-const sensor = spawn('python', ['sensor.py']);
-sensor.stdout.on('data', function (data) {
-
-    temperatures.push(parseFloat(data));
-    console.log(temperatures);
-});
