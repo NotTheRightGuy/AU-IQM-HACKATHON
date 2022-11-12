c2c23246f754d523d1edffd02e3d3bc03d159d7e
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2022-11-13 04:43:25 +0530

    Added Backend Support

--- server/server.js
+++ server/server.js
@@ -1,17 +1,55 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
 const cors = require("cors");
-const { json } = require("express");
-app.use(cors());
-app.use(express.json());
 
-app.post("/api/intern/register", (req, res) => {
-    // res.send("Intern Registered");
-    console.log(req.body);
-    res.json({ status: "ok" });
+app.use(express.json());
+app.use(cors());
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+mongoose.connect(
+    `mongodb://localhost:27017/intern-list`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if (!err) console.log("Connected to database");
+        else console.log("Couldn't connect to database");
+    }
+);
+
+const userSchema = new mongoose.Schema({
+    name: { String, unique: true },
+    mail: { String, unique: true },
+    password: { String, unique: true },
 });
 
-app.listen(`${PORT}`, () => {
-    console.log(`Listening on port ${PORT}`);
+const user = mongoose.model("user-data", userSchema);
+
+app.post("/api/intern/new-user", async (req, res) => {
+    const newUser = new user({
+        name: req.body.name,
+        mail: req.body.mail,
+        password: req.body.password,
+    });
+    try {
+        await newUser.save();
+        console.log("New user added");
+    } catch (err) {
+        console.log("User Already Exists");
+    }
+});
+
+app.get("/api/intern/list", (req, res) => {
+    console.log("GET request received");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running at port 3000");
 });
